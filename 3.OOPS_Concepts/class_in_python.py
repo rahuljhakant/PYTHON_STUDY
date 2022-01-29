9dc0c22bc4b0abe5de77e33b1ce586d68dc33d2d
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-01-29 22:28:40 +0530

    class in python

---
+++ 3.OOPS_Concepts/class_in_python.py
@@ -0,0 +1,12 @@
+class myClass:
+    num = 1, 2, 3
+    sum = 1, 2, 1
+
+    def __init__(self, a, b):
+        return self.a, self.b
+
+    def add(self, a, b):
+        return self.a + self.b
+
+    def sub(self, a, b):
+        return self.a - self.b
