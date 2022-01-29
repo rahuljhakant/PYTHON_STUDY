e051539e87ea990073801612bd08dbf5e5f0125e
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-01-30 00:41:59 +0530

    constructor in python

--- 3.OOPS_Concepts/defining_constructor_method_in_class.py
+++ 3.OOPS_Concepts/defining_constructor_method_in_class.py
@@ -10,6 +10,23 @@ Object Initialisation
 
 __new__() method is used to create the object.
 __init__() method is used to initialize the object.
-
+__del__() method is used to destroy the object.
 
 """
+
+
+class constructor:
+    def __new__(self):
+        print("New is called when the object is created")
+
+    # def __init__(self, numInt):
+    #     self.numInt = numInt
+    #     print("object is initialized")
+
+    def __del__(self):
+        print("object is destroyed")
+
+
+obj = constructor();
+
+del obj;
