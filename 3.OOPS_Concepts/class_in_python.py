79196c8ff8d06f1a14308d5a21b08152152590e0
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-01-29 23:38:11 +0530

    class and objects in python

--- 3.OOPS_Concepts/class_in_python.py
+++ 3.OOPS_Concepts/class_in_python.py
@@ -1,12 +1,35 @@
-class myClass:
-    num = 1, 2, 3
-    sum = 1, 2, 1
+# Defining a class is simple, all you have to do is use the keyword class followed by the
+# name that you want to give your class, and then a colon symbol :
+class Apollo:
+    # define a variable
+    destination = "moon"
 
-    def __init__(self, a, b):
-        return self.a, self.b
+    # definig the member functions
+    def fly(self):
+        print("Spaceship flying...")
 
-    def add(self, a, b):
-        return self.a + self.b
+    def get_destination(self):
+        print("Destination is: " + self.destination)
 
-    def sub(self, a, b):
-        return self.a - self.b
+
+obj = Apollo()
+obj.fly()
+obj.get_destination()
+obj.destination = "earth"
+
+"""[About objects]
+    # Objects are created when you create a class.
+    # Objects are instances of a class.
+    # Objects have attributes.
+    # Objects have methods.
+    # Objects can be created using the class name.
+    # Objects can be created using the class name and passing arguments to the constructor.
+
+    1. We add the self parameter when we define a member function, but do not specify it while calling the function.
+    2. When we called get_destination function for objFirst it gave output as Destination is: mars, because we updated the value for the variable 
+    destination for the object objFirst.
+    3. To access a member function or a member variable using an object, we use a dot . symbol.
+    4. And to create an object of any class, we have to call the function with same name as of the class.
+    5. To call a member function of an object, we use the object name followed by a dot . and the member function name.
+    
+"""
