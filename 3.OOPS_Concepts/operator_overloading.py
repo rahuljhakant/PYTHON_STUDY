c3bb4debc66ba362784e282cfab1fa30146f39ad
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-01-30 18:21:00 +0530

    operator overloading

---
+++ 3.OOPS_Concepts/operator_overloading.py
@@ -0,0 +1,31 @@
+# Operator overloading in Python
+
+
+# This different behaviour of a single operator for different types of operands is called Operator Overloading.
+
+# Take a look at the following snippet:
+
+a = 10
+b = 200
+
+print(a + b)
+
+# here + operator is used for addition of two numbers.
+
+
+c = "hello"
+d = "coco"
+
+print(c + d)
+
+# here + operator is used for concatenation of two strings.
+
+
+e = 10.99
+f = 20.99
+
+print(e + f)
+
+# here + operator is used for addition of two floats numbers.
+
+#  in the above snippet, we have used + operator for three different types of additions that is called operator overloading.
