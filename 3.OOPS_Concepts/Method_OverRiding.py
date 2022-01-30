2859a070914158afa817009589671ec82c401150
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-01-30 15:03:00 +0530

    method overriding in python

---
+++ 3.OOPS_Concepts/Method_OverRiding.py
@@ -0,0 +1,19 @@
+"""
+Method overriding is a concept of object oriented programming that allows us to change the implementation of a function
+in the child class that is defined in the parent class.
+
+"""
+
+"""
+Following conditions should be met for overriding a function:
+
+1. Inheritance should be there. Function overriding cannot be done within a class. We need to derive a child class from a parent class.
+2. The function that is redefined in the child class should have the same signature as in the parent class i.e. same number of parameters.
+
+
+
+As we have already learned about the concept of Inheritance, we know that when a child class inherits a parent class it also get access to it 
+public and protected(access modifiers in python) variables and methods.
+
+
+"""
