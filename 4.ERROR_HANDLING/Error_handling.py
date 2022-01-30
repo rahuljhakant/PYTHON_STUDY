621ccadd2263ac6c2f5fdbaf17fd3c6f24eef49f
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-01-30 18:41:15 +0530

    raising an exception

--- 4.ERROR_HANDLING/Error_handling.py
+++ 4.ERROR_HANDLING/Error_handling.py
@@ -11,11 +11,23 @@ class ExceptionHandle():
             a = 10
             b = 0
             print(a/b)
+
+        except ZeroDivisionError:
+            print("ZeroDivisionError")
         except Exception as e:
             print("Name of exception is: ", e)
+        finally:
+            print("Finally block is executed")
+
+    def rasing_Exception(self):
+        try:
+            raise NameError("Hi there")
+        except NameError:
+            print("An exception")
         finally:
             print("Finally block is executed")
 
 
 obj = ExceptionHandle()
 obj.division()
+obj.rasing_Exception()
