4961c795b996c36b77549abe711f9cddb89fb34e
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-02-01 15:08:43 +0530

    regex

--- 10.PYTHON_REGEX/Searching_an_occurrence_of_pattern .py
+++ 10.PYTHON_REGEX/Searching_an_occurrence_of_pattern .py
@@ -2,7 +2,8 @@
 re.search() :
 __________
 
-This method either returns None (if the pattern doesn’t match), or a re.MatchObject that contains information about the matching part of the string. This method stops after the first match, so this is best 
+This method either returns None (if the pattern doesn’t match), or a re.MatchObject that contains information about
+ the matching part of the string. This method stops after the first match, so this is best 
 suited for testing a regular expression more than extracting data.
 
 """
